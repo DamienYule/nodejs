77226a316031e22d16a6ab0b730b8ba2e9014273
DamienYule <47954095+DamienYule@users.noreply.github.com>
2020-03-20 11:25:44 -0400

    Routs

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('static'))
+
 app.get('/', (req, res) => {
-   res.send('<h1>Hire me! I can build you anything!</h1>')
+   res.send('<h1>I can build you anything!</h1>')
 
 })
+
+app.get('/about', (req, res) => {
+   res.send(`<h1>This is the art of Jäger contracting</h1>
+        <div>
+          <img src="/img/fusion.jpeg" >
+        </div> `)
+
+})
+
+app.get('/contact', (req, res) => {
+   res.send('<h1>damienyule@gmail.com</h1>')
+
+})
+
 
 app.listen(3000, () => {
   console.log('server is runnning on http://www.localhost:3000/ to stop server press control+c')
